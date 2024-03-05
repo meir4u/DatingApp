a177003f51a76e8f6d1077c2463c33eea59131cd
Meir Achildiev <meir.shopbroker@gmail.com>
2024-03-06 01:29:49 +0200

    added ngx-bootstrap

--- Api/DatingApp.Api/Program.cs
+++ Api/DatingApp.Api/Program.cs
@@ -13,11 +13,14 @@ namespace DatingApp.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-
+            
             builder.Services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            //cors
+            builder.Services.AddCors();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -36,6 +39,7 @@ namespace DatingApp.Api
 
             app.UseAuthorization();
 
+            app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.MapControllers();
 
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Dating App';
+export class AppComponent implements OnInit {
+  title: string = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:7269/api/Users').subscribe({
+      next: response => this.users = response,
+      error: (error) => {
+        console.log(error)
+      },
+      complete: () => { console.log('Request has completed!'); }
+    });
+    }
 }
