d0389bd68cac59b0a317a4a892120fdc5de09a30
Meir Achildiev <meir.shopbroker@gmail.com>
2024-03-10 20:57:16 +0200

    created Dtos with data migrations changes.

---
+++ Api/DatingApp.Api/DTOs/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Api.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [MinLength(3)]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
