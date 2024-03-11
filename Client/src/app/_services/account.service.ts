b59c4a7e13ef1f9e1f214fa3060b4e8750adda8d
Meir Achildiev <meir.shopbroker@gmail.com>
2024-03-11 12:25:24 +0200

    added model and account service

---
+++ Client/src/app/_services/account.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:7269/api/';
+  private currentUserSourse = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSourse.asObservable();
+  constructor(private http: HttpClient) { }
+
+  login(model: User) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model)
+      .pipe(
+        map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSourse.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSourse.next(user);
+
+        }
+        return user;
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSourse.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSourse.next(null);
+  }
+}
