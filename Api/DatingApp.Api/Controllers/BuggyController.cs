ac0a4e756633dd373b767e17af712110f845bdd4
Meir Achildiev <meir.shopbroker@gmail.com>
2024-03-11 23:26:08 +0200

    added buggy controller for testing

---
+++ Api/DatingApp.Api/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+﻿using DatingApp.Api.Data;
+using DatingApp.Api.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Api.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if (thing == null) return NotFound();
+
+            return thing;
+        }
+
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+            var thingsToReturn = thing.ToString();
+
+            return thingsToReturn;
+        }
+
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This is bad request");
+        }
+    }
+}
