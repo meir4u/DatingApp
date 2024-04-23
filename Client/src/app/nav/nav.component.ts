1120304493637ebddb7227e39cff97e3554c9a98
Meir Achildiev <meir.shopbroker@gmail.com>
2024-04-23 13:56:30 +0300

    but fix. when user logout the username and password shown in navbar fields.

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -25,7 +25,10 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: _ => this.router.navigateByUrl('/'),
+      next: _ => {
+        this.router.navigateByUrl('/');
+        this.model = {};
+      },
     })
     console.log(this.model);
   }
