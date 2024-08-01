2eb476a349da0790210a40ad16be784214e74160
Meir Achildiev <meir.shopbroker@gmail.com>
2024-08-02 01:52:11 +0300

    added DeleteMessage to GetMessagesForUserResponse

--- Api/Core/DatingApp.Application/Futures/Message/Responses/GetMessagesForUserResponse.cs
+++ Api/Core/DatingApp.Application/Futures/Message/Responses/GetMessagesForUserResponse.cs
@@ -1,4 +1,5 @@
-﻿using DatingApp.Application.Pagination;
+﻿using DatingApp.Application.DTOs.Message;
+using DatingApp.Application.Pagination;
 using DatingApp.Application.Responses;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,6 @@ namespace DatingApp.Application.Futures.Message.Responses
     public class GetMessagesForUserResponse : BaseCommandResponse
     {
         public PaginationHeader PaginationHeader { get; set; }
+        public PagedList<MessageDto> Messages { get; set; }
     }
 }
