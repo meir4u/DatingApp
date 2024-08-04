748979cce8928bb2e0bc8b15c84bb7355552d2e3
Meir Achildiev <meir.shopbroker@gmail.com>
2024-08-05 01:46:07 +0300

    code cleanup

--- Api/Core/DatingApp.Application/DTOs/Account/LoginDto.cs
+++ Api/Core/DatingApp.Application/DTOs/Account/LoginDto.cs
@@ -4,11 +4,7 @@ namespace DatingApp.Application.DTOs.Account
 {
     public class LoginDto
     {
-        //[Required]
-        //[MinLength(3)]
         public string Username { get; set; }
-        //[Required]
-        //[MinLength(6)]
         public string Password { get; set; }
     }
 }
