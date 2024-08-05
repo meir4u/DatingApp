1b25be76eb2f595a17aeeff00c6f473a968b5d8e
Meir Achildiev <meir.shopbroker@gmail.com>
2024-08-06 00:50:13 +0300

    create  handler exeption logging pipline behavior

--- Api/Core/DatingApp.Application/ApplicationServiceRegistration.cs
+++ Api/Core/DatingApp.Application/ApplicationServiceRegistration.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using System;
 using System.Reflection;
+using DatingApp.Application.Behaviors;
 
 namespace DatingApp.Application
 {
@@ -10,9 +11,13 @@ namespace DatingApp.Application
         public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            
             services.AddMediatR(cfg=> {
                 cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
                 });
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            
             return services;
         }
     }
---
+++ Api/Core/DatingApp.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,57 @@
+﻿using DatingApp.Application.Exceptions.Responses;
+using DatingApp.Common.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace DatingApp.Application.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger _logger;
+
+        public LoggingBehavior(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex) when (LogException(ex, request))
+            {
+                // This block will never be hit because the LogException method returns false
+                throw;
+            }
+        }
+
+        private bool LogException(Exception ex, TRequest request)
+        {
+            switch (ex)
+            {
+                case RepositoryException rex:
+                    _logger.Error(rex, "Repository exception: {message}", rex.Message);
+                    break;
+                case NotFoundException nfex:
+                    _logger.Warning(nfex, "Not found exception: {message}", nfex.Message);
+                    break;
+                case BadRequestExeption brex:
+                    _logger.Warning(brex, "Bad request exception: {message}", brex.Message);
+                    break;
+                default:
+                    _logger.Error(ex, "Unhandled exception: {message}", ex.Message);
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
